7f6374ff6cb373b8663face01a3c379008a3f7ac
Сергей Похабов <itigertechno@gmail.com>
2024-04-09 02:37:02 +0500

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import {
   MiddlewareConsumer,
   RequestMethod,
 } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthMiddleware } from './auth/middleware/auth.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,6 +14,10 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
