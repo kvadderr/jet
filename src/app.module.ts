c6ba6150cb9ea33806f7bccc045758bb66106a16
Сергей Похабов <itigertechno@gmail.com>
2024-04-09 02:20:52 +0500

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'Zsxdcf123',
+      username: 'postgres',
+      entities: [
+        User
+      ],
+      database: 'jet',
+      synchronize: true,
+      logging: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
